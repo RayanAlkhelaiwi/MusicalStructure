aa12c60d0e7f921661d883a33a8e6779841522dc
RayanAlkhelaiwi <r.n.m.k@live.com>
2017-08-16 13:58:15 +0300

    Initial commit

---
+++ app/src/main/java/me/rayanalkhelaiwi/musicalstructure/SongsList.java
@@ -0,0 +1,13 @@
+package me.rayanalkhelaiwi.musicalstructure;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SongsList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.songs_list);
+    }
+}
